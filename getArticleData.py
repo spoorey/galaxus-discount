b128309872db4fa66d390f5bb3b0966c1ff2aa7e
David Spörri <spoerridavid@gmail.com>
2020-03-17 00:01:12 +0100

    Use alternative API

--- getArticleData.py
+++ getArticleData.py
@@ -1,23 +1,22 @@
 import http.client
-import mimetypes
 import json
 
 def getArticleData(articleNumber):
     articleNumber = str(articleNumber)
     conn = http.client.HTTPSConnection("www.galaxus.ch")
-    payload = "{\"query\":\"query GET_INCREMENTAL_SEARCH_RESULT($query: String!) { incrementalSearch(query: $query) { searchResults { Suggestion { total results { name id } } ProductType { total results { id name } } Product { total results { name currency price insteadOfPrice id imageSource } } } } }\",\"variables\":{\"query\":\"" + articleNumber + "\"}}"
+    payload = "[{\"operationName\":\"GET_PRODUCT_DETAILS_CRITICAL_DATA_REFETCH\",\"variables\":{\"productId\":" + articleNumber + "},\"extensions\":{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"515fb14830e389b7bb81a5027dd0b6d44f7d1366fa2ebd5192f2e106be0bb230\"}}}]"
     headers = {
-    'Content-Type': 'application/json'
+        'content-type': ' application/json'
     }
     conn.request("POST", "/api/graphql", payload, headers)
     res = conn.getresponse()
     data = res.read()
     data = json.loads(data)
 
-    data = data['data']['incrementalSearch']['searchResults']['Product']
-    if (type(data) is not dict or len(data) != 2):
-        raise ValueError('Article with number ' + articleNumber + ' not found.')
-    data = data['results']
-    if (len(data) != 1):
-        raise ValueError('No unique article with number ' + articleNumber + ' not found.')
-    return data[0]
+    notFoundError = ValueError('Article with number ' + articleNumber + ' not found.')
+
+    try:
+        data = data[0]['data']['productDetails']
+    except TypeError:
+            raise notFoundError
+    return data
