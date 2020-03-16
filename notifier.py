a96da62e98530fcc23bc29d061e9a0e55360a0c3
David Spörri <spoerridavid@gmail.com>
2020-03-17 00:01:50 +0100

    Calculate discount and output to console

--- notifier.py
+++ notifier.py
@@ -1,6 +1,33 @@
 from getArticleData import getArticleData
 
+import yaml
 
-article = getArticleData('10401355')
+data = []
+with open('articles.yaml') as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
 
-print(float(article['price']) - float(article['insteadOfPrice']))
+for articleNr in data:
+    articleNr = str(articleNr)
+    try:
+        article = getArticleData(articleNr)
+    except ValueError as e:
+        print('Error: cannot find article #' + articleNr + ': ' + str(e))
+        continue
+    try:
+        amountIncl = article['product']['pricing']['price']['amountIncl']
+    except BaseException as e:
+        print('Error: cannot get price of article #' + articleNr + ': ' + str(e))
+        continue
+
+    amountBefore = amountIncl
+    try:
+        amountBefore = article['product']['pricing']['insteadOfPrice']['price']['amountIncl']
+    except TypeError:
+        amountBefore = amountIncl
+
+    discount = amountBefore - amountIncl
+    if discount == 0:
+        print('Article #' + articleNr + ' has no discount')
+        continue
+
+    print('Article #' + articleNr + ' has a discount of ' + str(discount) + ' CHF (' + str(round(discount/amountBefore, 2)*100) + '%)')
